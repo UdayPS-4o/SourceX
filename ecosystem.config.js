11e785c3d0e451fb464fd6cb3e7322dbfdf3b668
Uday <github@udayps.com>
2026-01-05 09:25:12 +0530

    deployment 1-fix-3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,22 +2,21 @@ module.exports = {
     apps: [
         {
             name: "sourcex-server",
-            script: "npm",
-            args: "start",
+            script: "./src/main.js",
+            args: "server",
             cwd: "./backend",
             instances: 1,
             autorestart: true,
             watch: false,
             max_memory_restart: "500M",
             env: {
-                NODE_ENV: "production",
-                PORT: 3000
+                NODE_ENV: "production"
             }
         },
         {
             name: "sourcex-monitor",
-            script: "npm",
-            args: "run monitor",
+            script: "./src/main.js",
+            args: "monitor",
             cwd: "./backend",
             instances: 1,
             autorestart: true,
